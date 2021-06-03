42a00ceb2a454486cf0467e9c7adae8d6d995106
Jun <lumjc@DESKTOP-G3BBFS3.localdomain>
2021-06-03 19:40:37 +0800

    routes done

---
+++ routes/authors.js
@@ -0,0 +1,45 @@
+const express = require ('express')
+const router = express.Router()
+const Author = require ('../models/author')
+
+
+// All Authors Route
+router.get('/authors', async (req, res) => {
+    let searchOptions = {}
+    if (req.query.name != null && req.query.name !== '') {
+      searchOptions.name = new RegExp(req.query.name, 'i')
+    }
+    try {
+      const authors = await Author.find(searchOptions)
+      res.render('authors/index', {
+        authors: authors,
+        searchOptions: req.query
+      })
+    } catch {
+      res.redirect('/')
+    }
+  })
+
+// New Author Route
+router.get('/authors/new', (req, res) => {
+    res.render('./authors/new' , { author: new Author() })
+})
+
+// create Author Route
+router.post('/authors' , async (req,res) => {
+    const author = new Author ({
+        name: req.body.name
+    })
+        try {
+            const newAuthor = await author.save()
+            res.redirect(`authors`)
+        } catch {
+            res.render('authors/new',{
+            author : author,
+            errorMessage : 'error'
+            })
+        }
+        // res.redirect(`authors/${newAuthor.id}`)
+})
+
+module.exports = router
--- server.js
+++ server.js
@@ -5,14 +5,21 @@ require('dotenv').config()
 const express = require('express')
 const app = express ()
 const expressLayouts = require ('express-ejs-layouts')
+const port = 3000
+const bodyParser = require('body-parser')
+
 
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
+
 
 app.set ('view engine','ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
+app.use(express.json())
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({limit: '10mb' , extended:false}))
 
 const mongoose = require ('mongoose')
 mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true , useUnifiedTopology: true })
@@ -22,5 +29,8 @@ db.on('error' , err => console.log(err))
 db.once('open',() => console.log('Connected to Mongoose'))
 
 app.use('/' , indexRouter)
+app.use('/', authorRouter)
 
-app.listen(process.env.PORT || 3002)
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+  })
